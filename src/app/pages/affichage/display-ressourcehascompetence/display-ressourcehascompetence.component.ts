9d494961816e2795028308e56465ae11df1a0de4
thomas <thomas.blangero@infotel.com>
2019-08-09 09:07:13 +0200

    Update display-ressourcehascompetence.component.ts

--- src/app/pages/affichage/display-ressourcehascompetence/display-ressourcehascompetence.component.ts
+++ src/app/pages/affichage/display-ressourcehascompetence/display-ressourcehascompetence.component.ts
@@ -18,7 +18,7 @@ export class DisplayRessourcehascompetenceComponent implements OnInit {
   constructor(private rcService: RessourcehascompetenceService, private router: Router) { }
 
   ngOnInit() {
-    this.getRessourcehascompetences();
+    this.rcService.getAll().subscribe((rcs) => this.rcs = rcs);
   }
 
   onSelect(rc: Ressourcehascompetence): void {
@@ -34,59 +34,9 @@ export class DisplayRessourcehascompetenceComponent implements OnInit {
     this.router.navigate(['/createRessourcehascompetence'], {state: {rc: this.selected}});
   }
 
-  clear(): void {
-    this.rcService.clear().subscribe();
-  }
-
-  create(rc: Ressourcehascompetence): void {
-    this.rcService.create(rc).subscribe((rcc) => this.rcs.push(rcc));
-  }
-
   delete(): void {
-    this.rcService.delete(this.selected.id).subscribe(
+    this.rcService.delete(this.selected.idr, this.selected.idc).subscribe(
       (result) => this.router.navigate(['/displayRessourcehascompetence']));
-  }
-
-  update(rc: Ressourcehascompetence): void {
-    this.rcs = this.rcs.filter((c) => c.idr !== rc.idr && c.idc !== rc.idc);
-    this.rcs.push(rc);
-    this.rcService.update(rc).subscribe();
-  }
-
-  getRessourcehascompetences(): void {
-    this.rcService.getAll().subscribe((rcs) => this.rcs = rcs);
-  }
-
-  getById(idr: number, idc:number): void {
-    this.rcService.getById(idr, idc).subscribe((rc) => this.rcs.push(rc));
-  }
-
-  getByRessource(nom: string, prenom: string, equipe: string): void {
-    this.rcService.getByRessource(nom, prenom, equipe).subscribe((rcs) => this.rcs = rcs);
-  }
-
-  getByCompetence(nom: string): void {
-this.rcService.getByCompetence(nom).subscribe((rcs) => this.rcs = rcs);
-  }
-
-  getByNiveau(niveau: number, orgnom: string): void {
-    this.rcService.getByNiveau(niveau, orgnom).subscribe((rcs) => this.rcs = rcs);
-  }
-
-  getByRessourceAndCompetence(nom: string, prenom: string, equipe: string, cnom: string): void {
-    this.rcService.getByRessourceAndCompetence(nom, prenom, equipe, cnom).subscribe((rcs) => this.rcs = rcs);
-  }
-
-  getByDateEvol(date: Date): void {
-    this.rcService.getByDateEvol(date).subscribe((rcs) => this.rcs = rcs);
-  }
-
-  getByRessourceAndDateEvol(nom: string, prenom: string, equipe: string, date: Date): void {
-    this.rcService.getByRessourceAndDateEvol(nom, prenom, equipe, date).subscribe((rcs) => this.rcs = rcs);
-  }
-
-  getByCompetenceAndDateEvol(nom: string, date: Date): void {
-    this.rcService.getByCompetenceAndDateEvol(nom, date).subscribe((rcs) => this.rcs = rcs);
   }
 
   getColorButton(): string {
